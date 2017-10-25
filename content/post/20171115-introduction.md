69777326d8ecde81ad6739b65fe5315d0b611134
Aaron Berk (grimpoteuthis) <aberk@math.ubc.ca>
2017-10-25 12:04:31 -0700

    [IC] set of base files for 17-18 iteration

---
+++ content/post/20171115-introduction.md
@@ -0,0 +1,33 @@
+---
+title: "bcdata colloquium introduction"
+date: 2017-10-25T00:08:53-07:00
+---
+
+
+# 2017 bcdata colloquium
+
+
+## Organization and sponsorship
+Organizers include the Institute of Applied
+Mathematics (UBC IAM) and the Pacific Institute for the Mathematical Sciences (PIMS). 
+
+This series is co-sponsored by [CANSSI](http://www.canssi.ca),
+[UBC DSI](https://dsi.ubc.ca), [UBC IAM](http://www.iam.ubc.ca) and
+[PIMS](https://www.pims.math.ca/).
+
+<table style="width:100%">
+<tr style="text-align:center">
+<td>
+<img src="/img/canssi_logo.png" style="width:auto; max-height:100px;">
+</td>
+<td>
+<img src="/img/iam-logo.png" style="width:auto; max-height:100px;">
+</td>
+<td>
+<img src="/img/pims-logo.png" style="width:auto; max-height:100px;">
+</td>
+<td style="text-align: center;">
+<img src="/img/ubc-dsi-logo.png" style="width:auto; max-height:100px; align:right;">
+</td>
+</tr>
+</table>
---
+++ scripts/publi.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo -d public/2017
+
+cp CNAME ./public/
+cp -r favi/* ./public/
+cp index.redirect ./public/index.html
+
+# add 2016 content to 2016 folder
+
+shopt -s extglob
+rm -rf ../bcdata-deployed/!(.git)
+mv ./public/* ../bcdata-deployed/
+
+read -erp "Push gh-pages to origin? (y/n): " doPushToRemote
+
+if [[ "$doPushToRemote" == "y" ]]
+then
+    echo "Pushing to remote..."
+    read -erp "Commit Message: " commitMessage
+    cd ../bcdata-deployed && git add --all && git commit -m "$commitMessage" && git push origin gh-pages
+fi
